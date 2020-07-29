082c9b10f96fa0b23366bb35e1f642c982b4263e
Vitor716 <62679174+Vitor716@users.noreply.github.com>
2020-07-29 20:07:22 -0300

    drop-box
    need to fix the navbar and links

--- Pages/Home/scripts.js
+++ Pages/Home/scripts.js
@@ -7,3 +7,33 @@ function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
 	document.getElementById("main").style.marginLeft = "0";
 }
+
+function orangeAlert() {
+	classRemove("orangeAlert", "hide");
+	classRemove("orangeCircle", "hide");
+	classRemove("orangeCheck", "hide");
+	classRemove("orangeTitle", "hide");
+	classRemove("orangeMessage", "hide");
+
+	classAdd("orangeAlert", "transition");
+
+	setTimeout(() => {
+		classAdd("orangeAlert", "hide");
+	}, 2500);
+}
+
+function changeClass(id, classRemove, newClass) {
+	const targetElement = document.getElementById(id);
+	targetElement.classList.remove(classRemove);
+	targetElement.classList.add(newClass);
+}
+
+function classRemove(id, classRemove) {
+	const targetElement = document.getElementById(id);
+	targetElement.classList.remove(classRemove);
+}
+
+function classAdd(id, classAdd) {
+	const targetElement = document.getElementById(id);
+	targetElement.classList.add(classAdd);
+}
