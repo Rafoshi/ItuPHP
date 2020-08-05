77cf3e7ff232f5101a3fcf258437a99630b5b4d8
Vitor716 <62679174+Vitor716@users.noreply.github.com>
2020-08-04 21:54:20 -0300

    portifolio home

--- Pages/Home/scripts.js
+++ Pages/Home/scripts.js
@@ -64,3 +64,20 @@ function classAdd(id, classAdd) {
 	const targetElement = document.getElementById(id);
 	targetElement.classList.add(classAdd);
 }
+$(function () {
+	var selectedClass = "";
+	$(".fil-cat").click(function () {
+		selectedClass = $(this).attr("data-rel");
+		$("#portfolio").fadeTo(100, 0.1);
+		$("#portfolio div")
+			.not("." + selectedClass)
+			.fadeOut()
+			.removeClass("scale-anm");
+		setTimeout(function () {
+			$("." + selectedClass)
+				.fadeIn()
+				.addClass("scale-anm");
+			$("#portfolio").fadeTo(300, 1);
+		}, 300);
+	});
+});
