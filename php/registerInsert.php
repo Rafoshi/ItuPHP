cf9c9b1cc4bae3e4f4bde9415ba63785ea5f5f9a
Rafael  Ioshi Imamura Pereira <rafa.ioshi@gmail.com>
2020-11-12 08:30:17 -0300

    Added the email verification

--- php/registerInsert.php
+++ php/registerInsert.php
@@ -9,15 +9,15 @@
 
     $consulta = $cn -> query("select user_email from tbl_user where user_email = '$email'");
     
-    if($consulta -> rowCount() == 1){
+    if($consulta -> rowCount() >= 1){
+        echo "<script> alert('E-mail inválido') </script>";
+    }
+    else{
         try {
             $query = $cn -> query("INSERT INTO `infityphp`.`tbl_user` (`user_name`, `user_email`, `user_password`, `user_lvl`) VALUES ('$name', '$email', '$password', '0')");
         }
         catch(Exception $e) {
             echo '<script>alert("Algo de errado aconteceu... tente novamente ou recarregue a página")</script>';
         }
-    }
-    else{
-        echo "<script> alert('E-mail inválido') </script>";
     }
 ?>
